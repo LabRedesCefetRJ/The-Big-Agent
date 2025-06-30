973edd117b77008a92ec4f46f3b51858188f1f97
daniel <danielfcosta247x@gmail.com>
2025-06-30 14:45:11 -0300

    Exemplo_Webots removido e atualização

--- Exemplos_Webots/Exemplo_Nardin-main/controllers/target/build/release/target.d
+++
@@ -1,7 +0,0 @@
-build/release/target.o: target.c \
- /usr/local/webots/include/controller/c/webots/distance_sensor.h \
- /usr/local/webots/include/controller/c/webots/types.h \
- /usr/local/webots/include/controller/c/webots/motor.h \
- /usr/local/webots/include/controller/c/webots/robot.h \
- /usr/local/webots/include/controller/c/webots/nodes.h \
- /usr/local/webots/include/controller/c/webots/gps.h
--- Exemplos_Webots/FourWheels_With_ChonIDE_Webots-main/README.md
+++
@@ -1,2 +0,0 @@
-# FourWheels_With_ChonIDE_Webots
-A simulation of a four-wheeled vehicle and a distance sensor using ChonIDE and Webots

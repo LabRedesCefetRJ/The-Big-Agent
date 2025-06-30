f9fdef3ad27fd16ca71c58694fb4ff315e37fc00
daniel4244 <06168900799@cefet-rj.br>
2025-06-30 15:59:54 -0300

    The-Big-Agent V0.7

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/daniel4244/Projeto_Drone
-cd Projeto_Drone/Projeto_Drone || exit
+git clone https://github.com/daniel4244/The-Big-Agent.git
+cd The-Big-Agent || exit
 
 sudo chmod 666 /dev/ttyExogenous0
-
-sudo chmod +x ./controllers/Drone_Controller_Test/Grindor
+sudo chmod +x ./controllers/Grindor.c
 
 make clean all
 make clean
 
 webots worlds/mundo.wbt 
-cd Projeto_Drone/MAS/droneMAS
